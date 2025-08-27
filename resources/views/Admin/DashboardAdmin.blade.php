9882beafbc236c5aa7d0b1f8ef20852fb6029d27
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2025-08-28 00:04:56 +0800

    update

--- resources/views/Admin/DashboardAdmin.blade.php
+++ resources/views/Admin/DashboardAdmin.blade.php
@@ -1,11 +1,99 @@
 @extends('Admin.template.main')
 
 @section('judul')
-        Dashboard
+    Dashboard
 @endsection
+
 @section('Judulisi')
     <h2>Dashboard</h2>
 @endsection
+
 @section('Content1')
-    
+<div class="container my-4">
+    <div class="card shadow rounded-3">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <h5 class="fw-bold">Duta Utama Grafika</h5>
+                    <p class="mb-0">Jl TK Irawadi 66 Panjer, Denpasar</p>
+                    <p class="mb-0">Telp: 0361 244061, 8083976, 8955186</p>
+                </div>
+                <div class="col-6 text-end">
+                    <p class="mb-0">Denpasar, 21 Sep 2016</p>
+                    <p class="mb-0">No Nota: <strong>10090096</strong></p>
+                    <p class="mb-0">Kpd Yth: <strong>Arti Printing</strong></p>
+                </div>
+            </div>
+            
+            <hr>
+
+            <table class="table table-bordered table-sm align-middle">
+                <thead class="table-light">
+                    <tr>
+                        <th style="width: 5%;">No</th>
+                        <th>Nama Barang</th>
+                        <th style="width: 10%;">Qty</th>
+                        <th style="width: 20%;">Harga</th>
+                        <th style="width: 20%;">Jumlah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>TAS GAHARU MERAH 40 BLOCK 200</td>
+                        <td>1</td>
+                        <td>150,000.00</td>
+                        <td>150,000.00</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>DOF 1 SISI TAS GAHARU</td>
+                        <td>1</td>
+                        <td>480,000.00</td>
+                        <td>480,000.00</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>PLONG TAS GAHARU 1000</td>
+                        <td>1</td>
+                        <td>50,000.00</td>
+                        <td>50,000.00</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>PLATE 52</td>
+                        <td>1</td>
+                        <td>20,000.00</td>
+                        <td>20,000.00</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="row mt-3">
+                <div class="col-6">
+                    <p><strong>Penerima</strong></p>
+                    <br><br>
+                    <p>_____________________</p>
+                </div>
+                <div class="col-6 text-end">
+                    <table class="table table-borderless">
+                        <tr>
+                            <th>Total</th>
+                            <td class="text-end">700,000.00</td>
+                        </tr>
+                        <tr>
+                            <th>Deposit</th>
+                            <td class="text-end">700,000.00</td>
+                        </tr>
+                        <tr>
+                            <th>Sisa</th>
+                            <td class="text-end">0</td>
+                        </tr>
+                    </table>
+                    <p class="mt-4">Duta Utama Grafika</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
