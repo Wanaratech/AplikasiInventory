eb5833720b34bce2781fbe67a897febab2050422
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2025-09-19 23:41:58 +0800

    Update DataPembelian.blade.php

---
+++ resources/views/Admin/Pembelian/DataPembelian.blade.php
@@ -0,0 +1,11 @@
+@extends('Admin.template.main')
+
+@section('judul')
+       Pembelian Barang
+@endsection
+@section('tittleCard')
+    <h2>Pembelian Barang</h2>
+@endsection
+@section('Content1')
+
+@endsection
